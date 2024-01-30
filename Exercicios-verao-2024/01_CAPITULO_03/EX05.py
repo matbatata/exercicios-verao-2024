5bbdf9a2fea5792700d72ce96c99f9ec95b59de1
Matheus <142700606+matbatata@users.noreply.github.com>
2024-01-30 14:13:54 -0300

    Add files via upload

---
+++ Exercicios-verao-2024/01_CAPITULO_03/EX05.py
@@ -0,0 +1,7 @@
+a = int(input('Digite um numero: '))
+
+if (a%2) == 0:
+        print("Par")
+else:
+        print("Ímpar")
+
---
+++ Exercicios-verao-2024/02_CAPITULO_04/1ex28.py
@@ -0,0 +1,9 @@
+n = int(input("digite um numero: "))
+i = 0 
+if n<0:
+    print("não aceitamos numeros negativos")
+else:
+    while n <= i:   
+        print(i)
+        i+=1
+    print("Fim!")
