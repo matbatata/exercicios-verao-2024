5aab4a550c9c9f6a0e8e4f6a40e7cd171baff2d7
Matheus <142700606+matbatata@users.noreply.github.com>
2024-02-09 20:08:16 -0300

    upload de todas as minha liçoes
    Upload de todas as lições|obs:ajeitei todos os nomes das liçoes

---
+++ Exercicios-verao-2024/01_CAPITULO_03/EX23.py
@@ -0,0 +1,4 @@
+num = input("Digite um número:\n")
+raiz = float(num) ** 0.5
+print(f'\nA raiz quadrada de {num} é {raiz}')
+
---
+++ Exercicios-verao-2024/02_CAPITULO_04/EX08.py
@@ -0,0 +1,7 @@
+a = int(input("digite um número: "))
+resto = a % 2
+
+if resto == 0:
+    print("o numero é par!")
+else:
+    print("o numero é impar!")
