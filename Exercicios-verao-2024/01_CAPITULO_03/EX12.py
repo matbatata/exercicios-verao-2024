18f45d2dad244ea61f6781f6fdd1e6ec04a4703f
Matheus <142700606+matbatata@users.noreply.github.com>
2024-02-27 20:39:38 -0300

    Update and rename EX10.py to EX12.py

---
+++ Exercicios-verao-2024/01_CAPITULO_03/EX12.py
@@ -0,0 +1,7 @@
+raio = int(input("digite  raio: "))
+altura = int(input("digite a altura: "))
+pi = 3.141592
+print(pi*raio**2*altura)
+
+#certo! parabéns! 
+# oq eu mudaria: print(pi*pow(raio,2)*altura)
