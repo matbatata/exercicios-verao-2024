fadb620fa2354c7251e4059814eb5c33e43a4dba
Matheus <142700606+matbatata@users.noreply.github.com>
2024-02-27 20:46:25 -0300

    Update EX04.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX04.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX04.py
@@ -1,7 +1,12 @@
-a = int(input("digite seu salario: "))
+a = int(input("digite seu salario: ")) 
 if 400<1200:
     print("emprestimo não concedido")
 elif 1200>400:
     print("emprestimo concedido")
 else:
     print("salário negado")
+
+#não usou 20%
+#não usou o a
+# revisar porcentagem
+#quase lá
