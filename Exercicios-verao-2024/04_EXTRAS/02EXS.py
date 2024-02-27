66d7d8b28c24af55cf9cb85fafb0d15f280732a9
Matheus <142700606+matbatata@users.noreply.github.com>
2024-02-27 20:57:35 -0300

    Update 02EXS.py

--- Exercicios-verao-2024/04_EXTRAS/02EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/02EXS.py
@@ -1,7 +1,9 @@
-n = 1
+n = 10  #deveria ter um int input
 soma = 0
 while n <= 10:
      x = int(input("Digite o %d número:"%n))
-     soma = soma + x
-     n = n + 1
-print("Soma: %d"%soma)
+     soma += x
+     n += 1
+print("Soma: %d"%, soma)
+
+#mt bom
