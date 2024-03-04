51a4aceeea15b8e3fca39d3f0a4fee96d31e28a8
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-04 19:00:25 -0300

    Update EX06.py

--- Exercicios-verao-2024/01_CAPITULO_03/EX06.py
+++ Exercicios-verao-2024/01_CAPITULO_03/EX06.py
@@ -1,9 +1,4 @@
-metros = int(input("Quantos kilometros? "))
-
-kilometros = metros * 1000
-
+k = int(input("Quantos kilometros? "))
+mentros = k/36
 print (metros, "m")
 
-# 1. não está lendo o valor de velocidade
-# 2. vc fez uma conversão de km para metro e não de km/h para m/s
-# 3. veja a fórmula do livro M = K/36
