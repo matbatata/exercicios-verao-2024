69a85b73b5ce0804a39e63c7fe91ddb7ee7b2f2b
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-07 21:01:15 -0300

    Update 03EXS.py

--- Exercicios-verao-2024/04_EXTRAS/03EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/03EXS.py
@@ -1,9 +1,10 @@
-peso = float(input('Digite seu peso (kg): '))
-if peso>80:
-    print("acima do peso")
-elif peso<80:
-    print("peso normal")
+peso = float(input("digite seu peso: "))
 
-# corrigir o caso 80kg
-# números negativos ... peso negativo?
-# dica: usar IF para não permitir que o usuário coloque números negativos
+if peso<0:
+    print("peso negativo")
+elif peso<=80:
+    print("peso normal")
+elif peso>=80:
+    print("acima do peso")
+#corrigido com sucesso! 
+#graças ao coach eu consegui fazer o codigo
