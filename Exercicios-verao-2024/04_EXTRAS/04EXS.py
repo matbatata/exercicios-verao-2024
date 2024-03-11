1bdf50a1497f225b205107e659eb8e3e0b1131b8
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-03-11 19:19:32 -0300

    CORREÇÃO

--- Exercicios-verao-2024/04_EXTRAS/04EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/04EXS.py
@@ -2,16 +2,17 @@
 a = int(input("SUB 1 SOMA 2 escolha uma opção: "))
 b = int(input("digite um numero: "))
 c = int(input("digite mais um numero: "))
-#soma e sub para fazer mais e menos
+
 soma = a+b
 sub = a-b
-#if e elif muito importantes nesse codigo
-if a < 0:
+
+
+if a < 0: # intenção foi boa, mas eh bom usar o else aqui
     print("numero negativo")
-elif a <= 1:
+elif a <= 1: # corrigir
     print(a-b)
-elif a >= 2:
+elif a >= 2: # corrigir 
     print(b+c)
 
-#corrigido
-#muito bom
+# Para pensar... e se na hora que eu colocasse o A, eu colocasse o numero 5 ou 7??
+#O programa rodaria errado
