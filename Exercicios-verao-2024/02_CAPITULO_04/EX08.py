ecbb17e00149b3bcf8ad25bb22583d8cc4a85e26
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-11 19:02:45 -0300

    Update EX08.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX08.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX08.py
@@ -1,15 +1,15 @@
 a = int(input("digite sua idade: "))
-idade = 70
-if a<8:
+if a < 0:
+    print("idade negativa")
+elif a<=8:
     print("você se encaixa no infantil A")
-elif a<9:
+elif a<=9:
     print("você se encaixa no infantil B")
-if a<10:
+elif a<=10:
     print("você se encaixa no juvenil A")
-elif a<14:
+elif a>=14:
     print("você se encaixa no juvenil B")
-if a>16:
+elif a<=16:
     print("você se encaixa no sênior ")
 
-# é simples de corrigir
-# dica: use <, <=, >=, > <-------- USAR MAIS DE UMA VEZ
+
