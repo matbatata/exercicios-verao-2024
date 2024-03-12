569be4bcf88e37b68f6ebc36958f49e20833816f
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-12 13:41:06 -0300

    Update EX04.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX04.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX04.py
@@ -1,11 +1,8 @@
 salario = float(input("digite seu salario: ")) 
-media = 1500
+emprestimo = float(input("digite seu empréstimo: ")) 
 porcentagem = salario*(20/100)
-if salario < 0:
-    print("emprestimo negado")
-elif salario>=media:
-    print("emprestimo concedido")
-elif salario<=media:
-    print("emprestimo negado ")
 
-# Nao precisava usar média... era para usar porcentagem.
+if emprestimo > porcentagem:
+    print("Empréstimo não concedido")
+else:
+    print("Empréstimo concedido")
