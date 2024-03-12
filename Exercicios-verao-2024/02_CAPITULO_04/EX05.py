292979d9690b23f22a8acdc4f6398f2db9381475
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-12 14:17:19 -0300

    Update EX05.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX05.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX05.py
@@ -1,1 +1,8 @@
-# NÃO COPIAR RESPOSTAS DA INTERNTET
+import math
+
+a = int(input("digite um numero: "))
+if a < 0:
+    print("numero é negativo")
+else:
+    print("RAIZ QUADRADA: ", math.sqrt(a))
+    print("AO QUADRADO: ", math.pow(a,2))
