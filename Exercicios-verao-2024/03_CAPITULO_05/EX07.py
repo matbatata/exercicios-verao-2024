3d009ce653bc0647fe450a055b5948c98a827833
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-12 13:42:54 -0300

    Update EX07.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX07.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX07.py
@@ -1,14 +1,10 @@
-a = int(input("digite 10 numeros aleatoriamente: "))
-b = int(input("numero: "))
-c = int(input("numero: "))
-d = int(input("numero: "))
-e = int(input("numero: "))
-f = int(input("numero: "))
-g = int(input("numero: "))
-h = int(input("numero: "))
-i = int(input("numero: ")) 
-j = int(input("numero: "))
-soma = a+b+c+d+e
-print(soma+f+g+h+i+j)
+n = 10 
+soma = 0
+contador = 1
+while contador <= 10:
+    x = int(input("Digite um número: "))
+    soma += x
+    contador+=1
+print("Soma: ", soma)
 
-# refazer
+#CORRIGIDO
