cf44007bcb8cca929dbc3e2efd0e6330380765b2
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-12 13:31:04 -0300

    Update EX08.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX08.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX08.py
@@ -1,11 +1,13 @@
 a = int(input("digite sua idade: "))
-if a <= 0:
-    print("idade negada")
-elif a<=9:
-    print("juvenil")
-elif a<=10:
-    print("infantil")
-else:
-    print(a>16,"senior")
-
-#refis ele e reduzi para uma idade real
+if a <= 0 or a > 80:
+    print("idade  invalida")
+elif 5 <= a <= 7:
+    print("infantil A")
+elif 8 <= a <= 10:
+    print("infantil B")
+elif 11 <= a <= 13:
+    print("juvenil A")
+elif 14 <= a <=17:
+    print("juvenil B")
+elif a>=18:
+    print("Senior")
