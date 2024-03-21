0ec97287f61b915927499f25e959c343be09f237
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-03-20 22:17:55 -0300

    Update EX13.py

--- Exercicios-verao-2024/01_CAPITULO_03/EX13.py
+++ Exercicios-verao-2024/01_CAPITULO_03/EX13.py
@@ -1,5 +1,12 @@
 import math 
-a = int(input("digite um numero: "))
-b = int(input("digite mais um numero: "))
+a = int(input("Digite o valor do primeiro cateto: "))
+b = int(input("Digite o valor do segundo cateto: "))
 h = math.sqrt(a**2+b**2)
 print(h)
+
+#Ccorrigido
+
+#Observação 01: Número tem acento agudo do U NÚ-ME-RO tomar cuidado com acentuação
+
+# Observação 02: Ao invés de colocar "insira um número", achei melhor colocar "insira o valor do primeiro e segundo cateto"
+
