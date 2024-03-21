9b174c75dc9a7fb05728c9ecf6f60b186e0d46f3
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-03-20 22:23:19 -0300

    Update EX02.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX02.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX02.py
@@ -7,3 +7,6 @@ while i < n:
   print(ímpar)
   i = i + 1
   ímpar = ímpar + 2
+
+# refazer....
+# exercício pedia ordem DECRESCENTE, ou seja, se n fosse 10 imprimiria: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
