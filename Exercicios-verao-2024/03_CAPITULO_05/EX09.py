e15be3ebf787a5737ccce51777fd4ec543e51040
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-27 13:30:07 -0300

    upload de novo ex
    Basicamente, o exercício 09 do capi 05

--- Exercicios-verao-2024/03_CAPITULO_05/EX09.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX09.py
@@ -1,19 +1,8 @@
-contador = 4
-i = 0
-while i <= contador:    
-    n = int(input("digite 5 numeros: "))
-    break
-if n == 0 and n < 0:
-        print("numero invalido")
-    
-elif n >= 1:
-        print(n,"maior que",1)
-elif n >= 2:
-    print(n,"maior que",2)
-elif n >= 3:
-        print(n,"maior que",3)
-elif n >= 4:
-        print(n,"maior que",4)    
-elif n >= 5:
-        print(n,"maior que",5)
-# era para usar while e não condiz com o enunciado
+contador = 1
+maior = int(input("Digite um número: ")) 
+while contador <= 9: 
+    x = int(input("Digite um número: "))
+    if maior < x:
+        maior = x 
+    contador+=1
+print("O maior número e: ",maior)
