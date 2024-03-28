38c9a7ac2a73c389d3b0ce730ef805fe53910925
Matheus <142700606+matbatata@users.noreply.github.com>
2024-03-28 14:40:47 +0000

    Update EX08.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX08.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX08.py
@@ -2,11 +2,9 @@ soma = 1
 contador = 0
 i = 1
 media = soma/i
-x = int
 while contador < 5:
-    print(input("digite 5 numeros: "))
+    x = int(input("digite 5 numeros: "))
     i+=1
     contador+=1
 
-print(media)
-# ?
+print(media/x)
