09e9e822cce026d220a411358dc53542d3fdf2de
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-03-29 20:16:21 -0300

    Update EX07.py

--- Exercicios-verao-2024/01_CAPITULO_03/EX07.py
+++ Exercicios-verao-2024/01_CAPITULO_03/EX07.py
@@ -2,5 +2,4 @@ reais = float(input("Informe a quantidade de reais para conversão em US$ "))
 contacao = 4.93 # dia 27 de fev
 print (reais*contacao)
 
-# 1. nao usar 'ç' em codigo
-#w. corrigido
+# corrigido
