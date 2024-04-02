fa4dd835b41ef4c99ad5ab3f3fd21c29718c431c
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-04-02 20:32:54 -0300

    Update EX07.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX07.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX07.py
@@ -1,20 +1,24 @@
-v = float(input("Digite quantas mercaria quer transportar: "))
+v = float(input("Digite o valor das mercadorias: "))
 print("1:MG")
 print("2:SP")
 print("3:RJ")
 print("4:MS")
 e = int(input("Escolha qual estado quer entregar: "))
-Minas = v*1.07
-São_paulo = v*1.12
-Rio_de_janeiro = v*1.15
-Mato_grosso_sul = v*1.08
+
+mg = round(v*1.07,2)
+sp = round(v*1.12,2)
+rj = round(v*1.15,2)
+ms = round(v*1.08,2)
+
 if v < 0 and e < 0:
     print("Erro")
 elif e == 1:
-    print(Minas)
+    print(mg)
 elif e == 2:
-    print(São_paulo)
+    print(sp)
 elif e == 3:
-    print(Rio_de_janeiro)
+    print(rj)
 elif e == 4:
-    print(Mato_grosso_sul)
+    print(ms)
+
+#corrigido
