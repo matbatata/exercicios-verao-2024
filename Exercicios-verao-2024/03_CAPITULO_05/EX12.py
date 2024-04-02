0bf38703fca576c3390d2ec704ed7871df439a88
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-04-02 20:49:10 -0300

    Update EX12.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX12.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX12.py
@@ -1,12 +1,12 @@
-soma = 1
-i = 1
-contador = 0
+div = 1
+contador = 1
+soma = 0
 x = int(input("Digite um número: "))
 while contador <= x:
     contador+=1
-    if x%i == 0:
-        print(soma+contador)
-    i+=1
+    if x%div == 0:
+        soma+=div
+    contador+=1
+    div+=1
 
-
-    
+print("Soma:", soma)
