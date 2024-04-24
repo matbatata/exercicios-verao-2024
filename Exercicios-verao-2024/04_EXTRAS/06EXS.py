61fbd12d83ea95a2f1d7350101c2c285f979a5ef
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-04-24 14:17:12 -0300

    Update 06EXS.py

--- Exercicios-verao-2024/04_EXTRAS/06EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/06EXS.py
@@ -1,13 +1,15 @@
 i = 0
 n = int(input("Digite um numero: "))
+
 while True:
-    break
+    # falta alguma coisa aqui
+    break #não
 if n == 0:
-    print("Erro")
+    print("Erro") # arrumar esta condição
 elif n % 2 == 0:
         print("Par")
 else:
     print("Impar")
-i+=1
+i+=1 # o que o i está fazendo?
 
 #arrumar
