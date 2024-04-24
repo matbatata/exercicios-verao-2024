9ddb765d9672d46b5fc35aaf4203d3aff573da61
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-04-24 14:19:39 -0300

    Update 09EXS.py

--- Exercicios-verao-2024/04_EXTRAS/09EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/09EXS.py
@@ -6,7 +6,9 @@ while True:
         break
     elif x  < 0:
         print(x,"É negativo")
-        contador_positivo+=1
+        contador_positivo+=1 # não era para contar os negativos?
     elif x > 0:
         print(x,"É positivo")
-        contador_positivo+=1
+        contador_positivo+=1 # não era para contar os negativos?
+
+# falta uma mensagem final mostrando quantos números positivos, negativos tiveram...
