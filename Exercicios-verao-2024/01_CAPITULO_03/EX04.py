73fed82a64cbff7f22f99c7a3c0547185729c4d4
Matheus <142700606+matbatata@users.noreply.github.com>
2024-04-30 18:23:17 -0300

    Add files via upload

---
+++ Exercicios-verao-2024/01_CAPITULO_03/EX04.py
@@ -0,0 +1,6 @@
+a = float(input("Digite sua nota de inglês: "))
+b = float(input("Digite sua nota de portugues: "))
+c = float(input("Digite um numero: "))
+d = float(input("Digite um numero: "))
+soma = a+b+c+d 
+print(soma / 5)
