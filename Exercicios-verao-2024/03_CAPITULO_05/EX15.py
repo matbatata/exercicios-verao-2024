c19d5f7cc84ea8f26656b9859fd18c7dae0b8144
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-01 19:24:48 -0300

    Add files via upload

---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX15.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input("Digite um número inteiro (ou um número negativo para terminar): "))
+    if n < 0:
+        break
+    elif n > maior:
+            maior = n
+    elif n < menor:
+            menor = n
+    print("O maior número é: ",maior)
+    print("O menor número é: ",menor)
