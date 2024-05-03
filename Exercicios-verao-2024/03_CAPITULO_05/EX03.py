97f38cbd71e5c8b8634ab55329224e8b41cc4664
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-03 16:33:15 -0300

    Upload de lições da sexta
    Lições escolhidas por eu mesmo

---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX03.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um número inteiro: "))
+contador = 0
+numero_impar = 1
+while contador < n:
+    print(numero_impar)
+    contador += 1
+    numero_impar += 2
---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX07.py
@@ -0,0 +1,7 @@
+soma = 0
+contador = 0
+while contador < 10:
+    numero = int(input("Digite um número: "))
+    soma += numero
+    contador += 1
+print("A soma dos números digitados é:", soma)
