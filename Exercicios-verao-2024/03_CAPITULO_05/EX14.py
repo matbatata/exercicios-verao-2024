bdcfc29cdd817718a7abeedae37a697c5d61e6cd
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-03 15:54:10 -0300

    ultimo exercício da semana
    Esse era o último que faltava da lição da semana

---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX14.py
@@ -0,0 +1,20 @@
+# Definindo as variáveis
+n = int(input("Defina o enésimo termo: "))
+contador = 1
+i = 0
+j = 1
+
+if n <= 0:
+    print("Número inválido")
+elif n == 1:
+    print(0)
+elif n ==2:
+    print(1)
+else:
+    while contador <= n-2:
+        k = j+i
+        i = j
+        j = k
+        contador+=1
+
+    print("O valor da sequência de Fibonacci é:", k)
