0242e2b405fbb86da9828b039fc91c432fe4145b
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-05 19:02:37 -0300

    Mais um exercício

---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX21.py
@@ -0,0 +1,15 @@
+qn = int(input("Quantos números você deseja colocar? "))
+if qn <= 0:
+    print("Quantidade inválida(O codigo funciona com números positivos)")
+else:
+    mn = float(input("Digite o primeiro número: "))
+    oc = 1
+    contador = 1
+    while contador < qn:
+        n = int(input("Digite um número: "))
+        if n > mn:
+            mn = n
+            oc = 1
+        elif n == mn:
+            oc+=1
+        contador+=1
