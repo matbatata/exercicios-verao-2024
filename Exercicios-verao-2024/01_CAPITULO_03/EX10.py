19f2f1955303f56b76d1b9d229ada83a35eb7b33
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-22 16:48:21 -0300

    EX10 está aqui
    Terminei mais um de muitos

---
+++ Exercicios-verao-2024/01_CAPITULO_03/EX10.py
@@ -0,0 +1,9 @@
+premio = 780000.00
+
+primeiro = premio * 0.46
+segundo = premio * 0.32
+terceiro = premio - primeiro - segundo
+
+print("Valor para o primeiro ganhador: R$", primeiro)
+print("Valor para o segundo ganhador: R$", segundo)
+print("Valor para o terceiro ganhador: R$", terceiro)
