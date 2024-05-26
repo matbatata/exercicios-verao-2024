36ae8ec8f222f7f6a1fd223d2349e9df875be076
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-26 18:47:20 -0300

    Update EX06.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX06.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX06.py
@@ -1,7 +1,14 @@
+p = int(input("Escolha uma opcão: "))
+print("1.homem")
+print("2.mulher")
 h = float(input("digite sua altura:  "))
 homens = (72.7*h)-58
 mulheres = (62.1*h)-44.7
-print(homens)
-print(mulheres)
+if h < 0 and p < 0 or p > 2:
+  print("erro")
+elif p == 1:
+  print(homens)
+elif p == 2:
+  print(mulheres)
 
-#precisa de um if
+#corrigido pelo mat
