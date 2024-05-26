11bb07faa53305808d54237c7ceaa4baab240cab
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-26 19:10:25 -0300

    Update EX11.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX11.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX11.py
@@ -1,23 +1,13 @@
+x = int(input("Digite um número: "))
 div = 1
 contador = 1
-x = int(input("Digite um número: "))
-while contador <= x:
-    contador+=1
-    if x%div == 0:
-        print(div)
-    contador+=1
-    div+=1        
 
-"""
-Falta o próprio número. Exemplo: quando x = 48, deveria imprimir:
-1
-2
-3
-4
-6
-8
-12
-16
-24
-48 
-"""
+while contador <= x:
+    if x % div == 0:
+        print(div)
+    div += 1
+    contador += 1
+
+print(x)
+
+#corrigido pelo mat
