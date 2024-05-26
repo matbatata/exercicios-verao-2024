ab519cd4f04ccae463043a10938ad2320c3e4ce9
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-26 19:40:57 -0300

    EX17 aqui
    Fiz esse exercício agradecendo a deus que achei um vídeo e um site do cara falando sobre o exercício

---
+++ Exercicios-verao-2024/03_CAPITULO_05/EX17.py
@@ -0,0 +1,12 @@
+n = int(input("Digite o número de linhas para o triângulo de Floyd: "))
+linha = 1
+num = 1
+
+while linha <= n:
+    coluna = 1
+    while coluna <= linha:
+        print(num)
+        num += 1
+        coluna += 1
+    print()
+    linha += 1
