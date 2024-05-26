b0145eb2c0cea4161f1b405c84b8cfe1e49443f1
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-26 19:21:59 -0300

    Update EX15.py

--- Exercicios-verao-2024/03_CAPITULO_05/EX15.py
+++ Exercicios-verao-2024/03_CAPITULO_05/EX15.py
@@ -1,12 +1,21 @@
-while True:
-    n = int(input("Digite um número inteiro (ou um número negativo para terminar): "))
-    if n < 0:
-        break
-    elif n > maior:
-            maior = n
-    elif n < menor:
-            menor = n
-    print("O maior número é: ",maior)
-    print("O menor número é: ",menor)
+n = int(input("Digite um número inteiro (ou um número negativo para parar: )"))
 
-# Não funciona :(, você deve definir maior e menor antes de usá-los
+if n < 0:
+    print("Nenhum número foi digitado.")
+else:
+    maior = n
+    menor = n
+
+    while True:
+        n = int(input("Digite um número inteiro (ou um número negativo para parar): "))
+        if n < 0:
+            break
+        if n > maior:
+            maior = n
+        elif n < menor:
+            menor = n
+
+    print("O maior número foi:", maior)
+    print("O menor número foi:", menor)
+
+#corrigido e aumentado pelo mat
