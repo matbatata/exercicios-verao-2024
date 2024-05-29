e3e62592dcd7cdc865ec60af7dbc469291599c84
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-29 14:21:34 -0300

    Update 10EXS.py

--- Exercicios-verao-2024/04_EXTRAS/10EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/10EXS.py
@@ -6,22 +6,25 @@ while True:
     print("4. Div")
     print("5. Sair")
     escolha = int(input("Escolha uma opção: "))
+    
     if escolha == 5:
         print("Saindo da calculadora")
         break
-    elif escolha in ("1,2,3,4"): # Não ensinei isso...
+    elif escolha == 1 or escolha == 2 or escolha == 3 or escolha == 4:  
         x = int(input("Digite um número: "))
         z = int(input("Digite mais um número: "))
-    elif escolha == 1:
-        print(x,"+",z = x+z)
-    elif escolha == 2:
-        print(x,"-",z = x+z)
-    elif escolha == 3:
-        print(x,"*",z = x*z)
-    elif escolha == 4:
-        print(x,"/",z = x/z)
-        # falta um IF aqui... 
+        if escolha == 1:
+            print("Soma:", x + z)
+        elif escolha == 2:
+            print("Subtração:", x - z)
+        elif escolha == 3:
+            print("Multiplicação:", x * z)
+        elif escolha == 4:
+            if z != 0:  
+                print("Divisão:", x / z)
+            else:
+                print("Erro: divisão por zero!")
     else:
-        print("Escolha invalida ")
+        print("Escolha inválida")
 
-    
+#corrigido pelo mat
