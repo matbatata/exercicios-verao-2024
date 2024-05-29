b22a9cf97749a905292599e617e0a3f9ed944a0b
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-29 14:14:53 -0300

    Update 09EXS.py

--- Exercicios-verao-2024/04_EXTRAS/09EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/09EXS.py
@@ -1,14 +1,18 @@
 contador_positivo = 0
 contador_negativo = 0
+
 while True:
-    x = int(input("Digite numeros inteiros: "))
+    x = int(input("Digite números inteiros: "))
     if x == 0:
         break
-    elif x  < 0:
-        print(x,"É negativo")
-        contador_positivo+=1 # não era para contar os negativos?
+    elif x < 0:
+        print(x, "é negativo")
+        contador_negativo += 1
     elif x > 0:
-        print(x,"É positivo")
-        contador_positivo+=1 # não era para contar os positivos?
+        print(x, "é positivo")
+        contador_positivo += 1
 
-# falta uma mensagem final mostrando quantos números positivos, negativos tiveram...
+print("Total de números positivos:", contador_positivo)
+print("Total de números negativos:", contador_negativo)
+
+#corrigido pelo mat
