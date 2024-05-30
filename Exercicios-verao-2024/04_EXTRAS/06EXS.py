b6ce35cd879f116478ea175248e312c302fceac1
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-30 19:23:35 -0300

    Update 06EXS.py

--- Exercicios-verao-2024/04_EXTRAS/06EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/06EXS.py
@@ -1,20 +1,20 @@
 i = 0
-n = int(input("Digite um número: "))
 
 while True:
+    n = int(input("Digite um número (ou 0 para sair): "))
+    
     if n == 0:
-        print("Erro")
-    elif n % 2 == 0:
+        print("Programa encerrado.")
+        break
+    
+    if n % 2 == 0:
         print("Par")
     else:
         print("Ímpar")
+    
     i += 1
-    break
 
 print("O loop foi executado", i, "vezes.")
 
-"""
-1. O que era para esse código fazer? O código é pra fazer um loop de impar ou par
-2. Sempre vai aparecer que o loop foi executado uma vez: vou consertar
-3. Acredito que falta pedir em cada iteração do loop para o usuário digitar um número: a consertar
-"""
+#Agora o código está certo
+#Um código de impar ou par com loop
