bde3ff4089348942afcc0c70794d0763181fc477
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-30 19:14:07 -0300

    Update 12EXS.py

--- Exercicios-verao-2024/04_EXTRAS/12EXS.py
+++ Exercicios-verao-2024/04_EXTRAS/12EXS.py
@@ -1,10 +1,11 @@
 while True:
     n = int(input("Digite um número: "))
     if n < 0:
-        print("0 Não é divisor") # na verdade é, reescreva esta frase com uma verdade
+        print("0 Não é divisivel")
         break
     if n % 5 == 0:
         print("O número",n,"é divisor de 5")
     else:
         print("o número",n,"não é divisor de 5")
-    
+
+#corrigido pelo mat
