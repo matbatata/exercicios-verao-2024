3b361c34b9260a0ca73869a1c24bae919aa2d65c
Matheus <142700606+matbatata@users.noreply.github.com>
2024-05-30 18:38:30 -0300

    Update EX09.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX09.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX09.py
@@ -1,24 +1,26 @@
-altura = float(input("digite sua altura: "))
-peso = float(input("digite seu peso: "))
-if altura <= 0 and peso <= 0:
-    print("dados invalidos")
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+
+if altura <= 0 or peso <= 0:
+    print("Dados inválidos")
 elif altura <= 1.20 and peso <= 60:
     print("A")
-elif altura >= 1.20 or altura <= 1.70 and peso <= 60:
+elif 1.20 < altura <= 1.70 and peso <= 60:
     print("B")
-elif altura >= 1.70 and peso <= 60:
+elif altura > 1.70 and peso <= 60:
     print("C")
-elif altura <= 1.20 and peso >= 60 or 90: # esse or 90 tá errado
+elif altura <= 1.20 and peso < 90:
     print("D")
-elif altura >= 1.20 and peso >= 60 or 90:
+elif 1.20 < altura <= 1.70 and peso < 90:
     print("E")
-elif altura >= 1.70 and peso >= 60 or 90:
+elif altura > 1.70 and peso < 90:
     print("F")
 elif altura <= 1.20 and peso >= 90:
     print("G")
-elif altura >= 1.20 or altura <= 1.70 and peso >= 90:
+elif 1.20 < altura <= 1.70 and peso >= 90:
     print("H")
-elif altura >= 1.70 and peso >= 90:
+elif altura > 1.70 and peso >= 90:
     print("I")
-
-# Eu tenho 1.74 e 75kg, eu deveria cair no F, mas cai no B.
+else:
+    print("Dado inválido")
+#corrigido pelo mat
