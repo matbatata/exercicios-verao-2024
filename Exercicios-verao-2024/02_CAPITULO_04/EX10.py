79284c444bcb1fc6755631b76604fe9253205170
Matheus <142700606+matbatata@users.noreply.github.com>
2024-06-03 19:56:57 -0300

    Update EX10.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX10.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX10.py
@@ -5,30 +5,23 @@ print("4 - ARITMÉTICA")
 
 opcao = int(input("Insira a opção: "))
 
-geometrica = pow(x*y*z, 1/3) # Esse daqui, eu forcei a barra. Foi mal
-ponderada = (x + 2*y + 3*z)/6 
-harmonica = 1/(1/x + 1/y + 1/z)
-aritmetica = (x+y+z) / 3
+x = float(input("Insira o valor de x: "))
+y = float(input("Insira o valor de y: "))
+z = float(input("Insira o valor de z: "))
 
-# Observação
-if opcao < 0 and opcao > 5:
-    print("Saiu") # ok 
+geometrica = pow(x*y*z,1/3)
+ponderada = (x+2*y+3*z)/6
+harmonica = 1 / (1/x + 1/y + 1/z)
+aritmetica = (x+y+z)/3
 
+if opcao < 1 or opcao > 4:
+    print("Opção inválida")
 elif opcao == 1:
-    # print(a*b*c) Você colocou a fórmula errada, mas valeu a intenção :)
     print(geometrica)
-
 elif opcao == 2:
-    #print(b+2*c+3*d/6) Nesta formúla, você esqueceu dos parênteses tomar cuidado...
     print(ponderada)
-
 elif opcao == 3:
-    #print(1//1/b+1/c+1/d) Esqueceu dos parênteses de novo...
     print(harmonica)
 elif opcao == 4:
-    #print(b+c+d/3) Precisava de parênteses... Quase certo! 
     print(aritmetica)
-elif opcao == 5:
-    print("SAIU") # essa linha não funciona... por quê? 
-
-# parabéns!!
+#corrigido pelo mat
