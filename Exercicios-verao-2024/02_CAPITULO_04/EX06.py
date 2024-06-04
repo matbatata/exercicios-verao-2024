aa042ba8108d2ce9722f8a4c3b1eb21456c0619c
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-04 11:32:34 -0300

    Update EX06.py

--- Exercicios-verao-2024/02_CAPITULO_04/EX06.py
+++ Exercicios-verao-2024/02_CAPITULO_04/EX06.py
@@ -1,14 +1,19 @@
-p = int(input("Escolha uma opcão: "))
+print("Escolha seu gênero: ")
 print("1.homem")
 print("2.mulher")
-h = float(input("digite sua altura:  "))
-homens = (72.7*h)-58
-mulheres = (62.1*h)-44.7
-if h < 0 and p < 0 or p > 2:
-  print("erro")
-elif p == 1:
-  print(homens)
-elif p == 2:
-  print(mulheres)
 
-#arrumar tópicos abordados no discord
+p = int(input("Escolha uma opcão: "))
+
+if p < 0 or p > 2:
+  print("Erro: Opção inexistente")
+else: 
+    h = float(input("digite sua altura:  "))
+    if h < 0:
+        print("Erro: Altura negativa")
+    else:
+        homens = (72.7*h)-58
+        mulheres = (62.1*h)-44.7
+        if p == 1:
+            print(round(homens,2))
+        else:
+            print(round(mulheres,2))
